df76064f9ea53a136ce76abc66a438e6c1291228
naingwin-aung <naingwinaung1710@gmail.com>
2021-12-15 14:58:54 +0630

    complete hamburger menu bar fade in and fade out animation

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,1 +1,15 @@
-console.log('hello world');
+const btnHamburger = document.getElementById('btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', function() {
+    if(header.classList.contains('open')) { //close Hamburger Menu
+        header.classList.remove('open');
+        overlay.classList.remove('fade-in');
+        overlay.classList.add('fade-out');
+    } else{ // open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.remove('fade-out');
+        overlay.classList.add('fade-in');
+    }
+})
